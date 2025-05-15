ec2506edef07585b45e10cb694db55f5e93e87e4
Suvir <suvirc6@gmail.com>
2025-05-16 04:46:00 +0530

    .

--- scrape_nse.js
+++ scrape_nse.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
   async function scrapeFinancials(tickers) {
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser',   // Adjust this path if necessary
+    // executablePath: '/usr/bin/chromium-browser',   // Adjust this path if necessary
     headless: true,
     args: [
       '--no-sandbox',
